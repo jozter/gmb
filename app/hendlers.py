980346ee775e761432ca48e43f0b3b6a21b88059
Veronika Shatunova <veronika.sshatunova@gmail.com>
2025-02-24 16:41:32 +0300

    Упрощение кода и правки

--- app/hendlers.py
+++ app/hendlers.py
@@ -1,90 +1,98 @@
-from aiogram import Router, F, Bot
+from aiogram import Router, Bot, F
 from aiogram.filters import Command
 from aiogram.types import Message, CallbackQuery
 from aiogram.enums import ParseMode
 import app.keyboards as kb
 from app.lexicon import lexicone as lx
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from config import TOKEN
 
 router=Router()
+bot = Bot(token=TOKEN)
 
-resp_id = None 
-resp_id1 = None
+message_ids = {}
 
 @router.message(Command(commands=["start"]))
 async def start(message: Message):
     await message.answer(text=lx['/start'], parse_mode=ParseMode.HTML, reply_markup=kb.menu)
     
-####################################КНОПКА СОРТИРОВКА###############################################
+###################     КНОПКА СОРТИРОВКА     ###############################################
 
 @router.message(F.text=='Сортировка в МИСИС')
 async def inline_sort(message: Message):
-    global resp_id
-    await message.answer(text='Раздельный сбор отходов', reply_markup=kb.set_sort) 
-    resp_message = await message.answer(text='Выбери место.')
-    resp_id = resp_message.message_id
+    chat_id = message.chat.id
+    await message.answer(text='Что ты хочешь сдать?', reply_markup=kb.set_sort) 
+    resp_message = await message.answer(text='Выбери фракцию.')
+    message_ids[chat_id] = resp_message.message_id
 
 @router.callback_query(lambda c: c.data.startswith('sort'))
 async def sortirovka(callback: CallbackQuery):
-    global resp_id
+    chat_id = callback.message.chat.id
     await callback.answer()
     response_text = lx.get(callback.data, "Неизвестный вариант")
-    if resp_id:
+    
+    if chat_id in message_ids:
         try:
             await callback.bot.edit_message_text(
                 text=response_text,
-                chat_id=callback.message.chat.id,
-                message_id=resp_id,
+                chat_id=chat_id,
+                message_id=message_ids[chat_id],
                 parse_mode=ParseMode.HTML
             )
         except Exception as e:
-            print(f"Error editing message: {e}")
+           print(f"Error editing message: {e}")
     else:
         sent_message = await callback.message.answer(response_text)
-        resp_id = sent_message.message_id
-       
-#################################НЕРАБОЧЕЕЕ########################################################## 
+        message_ids[chat_id] = sent_message.message_id
+###################     КНОПКА ЕСТЬ ВОПРОС     #############################################    
 
-@router.message(F.text=='Контейнер заполнен')
-async def start(message: Message):
-    await message.answer(text="Функция на доработке, скоро мы ее добавим. 💚")
-    
 @router.message(F.text=='Есть вопрос')
 async def start(message: Message):
-    await message.answer(text="Функция на доработке, скоро мы ее добавим. 💚")
-    
-###############################КНОПКА О НАС##############################################
-    
-@router.message(F.text=='О нас')
+    await message.answer(text="Если у тебя есть вопрос или предложение - пиши @twwdy. 🧚‍♀️")
+
+###################     КНОПКА О НАС     #######################################    
+
+@router.message(F.text == 'О нас')
 async def inline_sort(message: Message):
-    global resp_id1
-    await message.answer(lx.get('1about1'), reply_markup=kb.set_about, parse_mode=ParseMode.HTML) 
+    chat_id = message.chat.id
+    await message.answer(lx.get('1about1'), reply_markup=kb.set_about, parse_mode=ParseMode.HTML)
     resp_message1 = await message.answer(text='Выбери, что хочешь узнать.')
-    resp_id1 = resp_message1.message_id
+    message_ids[chat_id] = resp_message1.message_id  
 
 @router.callback_query(lambda c: c.data.startswith('about'))
 async def sortirovka(callback: CallbackQuery):
-    global resp_id1
+    chat_id = callback.message.chat.id
     await callback.answer()
     response_text = lx.get(callback.data, "Неизвестный вариант")
-    if resp_id1:
+
+    if chat_id in message_ids:
         try:
             await callback.bot.edit_message_text(
                 text=response_text,
-                chat_id=callback.message.chat.id,
-                message_id=resp_id1,
+                chat_id=chat_id,
+                message_id=message_ids[chat_id],
                 parse_mode=ParseMode.HTML
             )
         except Exception as e:
             print(f"Error editing message: {e}")
     else:
         sent_message1 = await callback.message.answer(response_text)
-        resp_id1 = sent_message1.message_id
-
-###############################КНОПКА УЗНАТЬ О СОТРИРОВКЕ##############################################
+        message_ids[chat_id] = sent_message1.message_id
+        
+###################     КНОПКА УЗНАТЬ О СОТРИРОВКЕ     ########################################
 
 @router.message(F.text=='Подготовка вторсырья')
 async def start(message: Message):
     await message.answer(lx.get('prepearing_waste'), parse_mode=ParseMode.HTML)
 
+###################     КНОПКА КОНТЕЙНЕР ЗАПОЛНЕН     ##################################################### 
+
+@router.message(F.text == "Контейнер заполнен")
+async def container_filled(message: Message):
+    await message.answer("Отправь фотографию контейнера, пожалуйста, а так же можешь добавить текст:")
+
+###################     АЙДИ ЧАТА     ########################################################### 
+
+@router.message(Command("chat_id"))
+async def cmd_chat_id(message: Message):
+    await message.answer(f"ID этого чата: {message.chat.id}")
 
--- app/keyboards.py
+++ app/keyboards.py
@@ -10,21 +10,12 @@ menu = ReplyKeyboardMarkup(keyboard=[
 
     
 set_sort = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='Точка кипения Комунна', callback_data='sort1')],
-    [InlineKeyboardButton(text='Корпус Б', callback_data='sort2')],
-    [InlineKeyboardButton(text='Горняк-1', callback_data='sort3')],
-    [InlineKeyboardButton(text='Металлург-3', callback_data='sort4')],
-    [InlineKeyboardButton(text='Медиацентр', callback_data='sort5')],
-    [InlineKeyboardButton(text='СтудОфис', callback_data='sort6')]
-    ])
-
-set_full = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='Точка кипения Комунна', callback_data='full1')],
-    [InlineKeyboardButton(text='Корпус Б', callback_data='full2')],
-    [InlineKeyboardButton(text='Горняк-1', callback_data='full3')],
-    [InlineKeyboardButton(text='Металлург-3', callback_data='full4')],
-    [InlineKeyboardButton(text='Медиацентр', callback_data='full5')],
-    [InlineKeyboardButton(text='СтудОфис', callback_data='full6')],
+    [InlineKeyboardButton(text='Крышечки от бутылок', callback_data='sort1')],
+    [InlineKeyboardButton(text='Батарейки', callback_data='sort2')],
+    [InlineKeyboardButton(text='Электронные сигареты', callback_data='sort3')],
+    [InlineKeyboardButton(text='Блистеры от лекарств', callback_data='sort4')],
+    [InlineKeyboardButton(text='Чеки', callback_data='sort5')],
+    [InlineKeyboardButton(text='Пластиковые стаканчики', callback_data='sort6')]
     ])
 
 set_about = InlineKeyboardMarkup(inline_keyboard=[
@@ -32,18 +23,3 @@ set_about = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='Социальные сети', callback_data='about2')],
     [InlineKeyboardButton(text='Вступить в клуб', callback_data='about3')],
     ])
-
-set_full_data = {
-    'full1': ['Стекло', 'Пластиковые бутылки', 'Макулатура', 'Батарейки', 'Крышки от бутылок', 'Пластиковые стаканчики'],
-    'full2': ['Крышки от бутылок', 'Электронки'],
-    'full3': ['Крышки от бутылок', 'Батарейки'],
-    'full4': ['Крышки от бутылок'],
-    'full5': ['Пластиковые стаканчики', 'Электронки', 'Батарейки'],
-    'full6': ['Чеки', 'Блистеры']
-}
-
-def generate_full_keyboard(callback_data: str):
-    keyboard = InlineKeyboardMarkup()
-    for item in set_full_data.get(callback_data, []):
-        keyboard.add(InlineKeyboardButton(text=item, callback_data=f"{callback_data}_{item.lower().replace(' ', '_')}"))
-    return keyboard
